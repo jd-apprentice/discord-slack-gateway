9198de575f1d8763074cebc1e75564f343a49665
Jonathan Dyallo <emeraldusk@gmail.com>
2023-06-14 02:05:05 -0300

    feat(license): updated license

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+/* Discord-Slack-Gateway -- A simple gateway to connect Discord and Slack
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Author: Jonathan Dyallo
+ * MIT License
+ *
+ * Copyright (c) 2023 Jonathan <contacto at jonathan dot com dot ar>
+ * 
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 const WebSocket = require('ws');
 const { WebhookClient } = require('discord.js');
 const { IncomingWebhook } = require('@slack/webhook');
@@ -6,8 +36,6 @@ const { Logger, IsBot } = require('./utils.js');
 
 /**
  * @author Jonathan Dyallo
- * @description DiscordSlackGateway is a library that allows you to connect a Discord channel to a Slack channel and vice versa.
- * @class DiscordSlackGateway
  * @example
  * const { DiscordSlackGateway } = require('discord-slack-gateway');
  * const gateway = new DiscordSlackGateway({
